f16cd802fedc4d4296c4b21717cac4dce3aa3116
Jordan Hudson <jordanhudson@gmail.com>
2026-01-20 18:13:12 -0800

    test fly github integration

--- src/server.ts
+++ src/server.ts
@@ -55,7 +55,6 @@ app.get('/movie/:id', async (c) => {
     return c.text('Movie not found', 404);
   }
 
-  // Get all screenings for this movie
   const screenings = await db
     .selectFrom('screening')
     .select([
